849a5495271a249922d1a97f81f4bb4e22dfef36
CodeStudent2002 <abdurashidovasrorbek7@gmail.com>
2024-06-26 09:11:22 +0300

    Add Program File

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Calculator
+
+{
+    class Program 
+    {
+        static void Main()
+        {
+            // Bu bir qatorli izoh
+            /// Exseptions
+            
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Asrorbek. Abdurashidov");
+            Console.WriteLine("2002-yil");
+
+           Console.ReadLine();
+            
+        }
+    }
+}
